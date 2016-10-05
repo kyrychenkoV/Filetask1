acf97372e35ace11c53800054aad5698940579a3
kyrychenkoV <keryvn@gmail.com>
2016-10-05 16:41:57 +0300

    new eror

--- main.c
+++ main.c
@@ -1,85 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-FILE * OpenFile(char *name,char *mode);
-void addNewFile(char * string,char *name,char *mode);
-void closeFile(FILE *name);
+FILE * input_Data(char *name,char *mode);
+FILE * addNewFile(char *name,char *mode);
 void findWordsWithNumbers();
+int functionisNumberInString (char string[][20]);
 int main() {
-    FILE *file;
-    char *inputdata="1ell1o my9 big Sity6 !";
-    addNewFile(inputdata,"textstring.txt","w");
-
-    file=OpenFile("city.txt","w");
-    fclose(file);
-
-    file=fopen("textstring.txt","r+");
-    char string[5][20];
-    int i=0;
-    int count2=0;
-    while(fscanf(file,"%s",string[i])!=EOF){
-        i++;
-
-        count2++;
-    }
-    fclose(file);
-
-    file=fopen("city.txt","r+");
-    i=0;
-    int j=0;
-
-    while (string[j]!='\0'){
-        if (string[i]>='0'&&string[i]<='9'){
-            fprintf(file,"%s",string[i]);
-            printf("%d",1);
-            i++;
-        }
-        j++;
-    }
-
-    printf("%s",string);
-    fprintf(file,"%s",string[1]);
-    fclose(file);
-    //findWordsWithNumbers();
+    findWordsWithNumbers();
     return 0;
 
 }
-FILE * OpenFile(char name[],char *mode){
+FILE * input_Data(char *name,char *mode){
     FILE *file=fopen(name,mode);
-    return file;
-
-}
-void closeFile(FILE *name){
-    fclose(name);
-
-}
-void addNewFile(char * string,char *name,char *mode){
-    FILE *file=fopen(name,mode);
-    fprintf(file,"%s ",string);
+    char *inputdata="Hello my9 big Sity6";
+    fprintf(file,"%s ",inputdata);
     fclose(file);
+    return file;
+}
+FILE * addNewFile(char *name,char *mode){
+    FILE *file=fopen(name,mode);
+    fclose(file);
+    return file;
 }
 void findWordsWithNumbers(){
-    //FILE * myfile ,*sity;
-    //myfile=input_Data("textstring.txt","w");
-    //sity=addNewFile("sity.txt","w");
+    FILE * myfile ,*sity;
+    myfile=input_Data("textstring.txt","w");
+    sity=addNewFile("sity.txt","w");
+    fclose(sity);
 
-    //myfile=fopen("textstring.txt","r");
-    //sity=fopen("sity.txt","r+");
-    //char string[20];
-    /*while(fscanf(myfile,"%s",string)!=EOF){
+    myfile=fopen("textstring.txt","r");
+    sity=fopen("sity.txt","a");
+    char string[5][20];
+    int caunt=0;
+    int i=0;
+    while(fscanf(myfile,"%s",string[i])!=EOF){
         int flag=0;
         int i=0;
-        while (string[i]!='\0') {
-            if (string[i]>='0'&&string[i]<='9'){
-                flag=1;
-            }
-            i++;
+        caunt++;
+        if (functionisNumberInString(string)==1){
+            fprintf(sity,"%s ",string[i]);
+
         }
-        if (flag==1){
-            fprintf(sity,"%s ",string);
-        }
+        i++;
     }
+    printf("%d",caunt);
     fclose(myfile);
-    fclose(sity);*/
+    fclose(sity);
+}
+int functionisNumberInString (char string[][20]){
+    int i=0;
+    while (string[i]!='\0') {
+        printf("%d",1);
+        if (string[i]>='0'&&string[i]<='9'){
+            printf("%d",2);
+            return 1;
+        }
+        i++;
+        return 0;
+    }
 
 }
